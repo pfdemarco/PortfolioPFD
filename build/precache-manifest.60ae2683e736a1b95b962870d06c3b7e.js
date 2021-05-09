d4affe891098c8ac116313edc6e51816265d4b8a
Patrick DeMarco <pfdemarco@hotmail.com>
2021-05-08 22:26:14 -0400

    added build of courwse it doesnt work

---
+++ build/precache-manifest.60ae2683e736a1b95b962870d06c3b7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93505c363115e66b22738fa4259ba872",
+    "url": "/pfdemarco/ReactPortfolioindex.html"
+  },
+  {
+    "revision": "d0fce24a385621a5da1d",
+    "url": "/pfdemarco/ReactPortfoliostatic/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7b9aed46bf01288a5884",
+    "url": "/pfdemarco/ReactPortfoliostatic/css/main.3a6235f4.chunk.css"
+  },
+  {
+    "revision": "d0fce24a385621a5da1d",
+    "url": "/pfdemarco/ReactPortfoliostatic/js/2.d7c9cdc5.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/pfdemarco/ReactPortfoliostatic/js/2.d7c9cdc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9aed46bf01288a5884",
+    "url": "/pfdemarco/ReactPortfoliostatic/js/main.95e64dd0.chunk.js"
+  },
+  {
+    "revision": "b58a5975fa23fa44bc76",
+    "url": "/pfdemarco/ReactPortfoliostatic/js/runtime-main.82c2ac1b.js"
+  }
+]);
